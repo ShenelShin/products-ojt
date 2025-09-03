2f25a89a4f382c27191b05550eb087553aa3af29
ShenelShin <shenelshincampo15@gmail.com>
2025-09-03 16:09:37 +0800

    Add files via upload

---
+++ Controllers/HomeController1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpApplication.Models;
+
+namespace SimpApplication.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context; 
+        public ProductController(AppDbContext context)
+        {
+            _context = context; 
+        }
+
+        
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList(); 
+            return View(products);
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SimpApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
